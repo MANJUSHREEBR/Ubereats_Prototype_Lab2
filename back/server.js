40a5c53cf13dc338defc89078ab53758d2ffe614
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-10-11 16:02:06 -0700

    Initial backend connection

---
+++ back/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('hello from node updated');
+})
+
+
+//defining port
+const port = process.env.PORT || 8000;
+app.listen(port,()=> console.log(`Server running on port ${port}`));
